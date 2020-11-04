04cb67a9829039181d95b024bccc131d8785e9d4
Garry-dhall <gdhall100@gmail.com>
2020-11-05 08:36:00 +1300

    Create new record of material

--- NUnitTestProject5/UnitTest1.cs
+++ NUnitTestProject5/UnitTest1.cs
@@ -31,7 +31,7 @@ namespace NUnitTestProject5
             //Click on Submit buttton
             driver.FindElement(By.CssSelector("input[type= submit]")).Click();
             Thread.Sleep(5000);
-
+            //Verifying Username on home page
            var x=  driver.FindElement(By.LinkText("Hello hari!")).Text;
            if(x == "Hello hari!")
             {
@@ -43,24 +43,33 @@ namespace NUnitTestProject5
             }
             //Click on Hello hari! dropdown
             driver.FindElement(By.LinkText("Hello hari!")).Click();
-            Thread.Sleep(5000);
-
-
-            // driver.FindElement(By.CssSelector("li [class= dropdown]a [data-toggle= dropdown]"));
-            
+            Thread.Sleep(5000);            
             //click on Administrator dropdown
             driver.FindElement(By.XPath("//*[contains(text(),'Administration')]")).Click();
             Thread.Sleep(5000);
-            
-
-
-
-
-
+            //click on Time & Materials
+            driver.FindElement(By.XPath("//*[contains(text(),'Time & Materials')]")).Click();
+            Thread.Sleep(5000);
+            //click on Create New Button
+            driver.FindElement(By.XPath("//*[contains(text(),'Create New')]")).Click();
+            Thread.Sleep(5000);
+            //Click on material dropdown
+            driver.FindElement(By.XPath("//span[text()='Material']")).Click();
+            Thread.Sleep(5000);
+            //Select material
+            driver.FindElement(By.Id("TypeCode_option_selected")).Click();
+            Thread.Sleep(5000);
+            //Enter code value
+            driver.FindElement(By.Id("Code")).SendKeys("pvc elbow");
+            //Enter description
+            driver.FindElement(By.Id("Description")).SendKeys("order new unit");
+            Thread.Sleep(5000);
+            //save the new created material details
+            driver.FindElement(By.Id("SaveButton")).Click();
+            Thread.Sleep(5000);
         }
         [TearDown]
-        
-
+       
         public void CloseDriver()
 
         { 
